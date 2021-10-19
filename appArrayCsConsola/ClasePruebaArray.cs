cc5878f60aa87dc686ec197c25547e1d49cd6aa5
Zurita <af3392976@gmail.com>
2021-10-19 09:22:41 -0500

    Add project files.

---
+++ appArrayCsConsola/ClasePruebaArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appArrayCsConsola
+{
+    class ClasePruebaArray
+    {
+        
+        private string[] arreglo = new string[5];
+
+        public string[] Arreglo
+        {
+            get { return arreglo; }
+            set { arreglo = value; }
+        }
+        private int indice;
+
+        public int Indice
+        {
+            get { return indice; }
+            set { indice = value; }
+        }
+
+        public ClasePruebaArray()
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            { arreglo[i] = ""; }
+            indice = 0;
+            
+        }
+        
+        public ClasePruebaArray(string valor, int indice)
+        {
+            this.indice = indice;
+            arreglo[indice] = valor;
+        }
+
+        public override string ToString()
+        { return "El indice " + indice + " contiene: " + arreglo[indice]; }
+
+    }
+}
---
+++ appArrayCsConsola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace appArrayCsConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Arreglo de 10 cadenas de caracteres
+            string [] arreglo = new string [10];
+            arreglo[0] = "uwu";
+            for(int i = 0; i < arreglo.Length; i++)
+            { arreglo[i] = "hola" + (i+1);  }
+            for (int i = 0; i < arreglo.Length; i++)
+            { Console.WriteLine(arreglo[i]); } */
+
+            Console.WriteLine("ingrese el valor de indice en el que desea escribir: ");
+            int indice = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese lo que quiere guardar en este indice: ");
+            string valor = Console.ReadLine();
+
+            ClasePruebaArray a1;
+            a1 = new ClasePruebaArray(valor, indice);
+            Console.WriteLine(a1.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
